a523d3da993cb9aca8bbc67d723d936b59b59818
Weronika Felczak <felczak.weronika@gmail.com>
2018-02-03 17:30:54 +0100

    Register form

--- views.py
+++ views.py
@@ -5,9 +5,25 @@ from main import db
 from main import bcrypt
 from main import lm
 
-from flask import render_template
+from models import User
+
+from flask import render_template, request, redirect
 
 
 @app.route('/', methods=['GET', 'POST'])
 def info():
     return render_template('info.html')
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/new-user', methods=['POST'])
+def new_post():
+    password = request.form['user_password']
+    email = request.form['user_email']
+    post = User(password=password, email=email)
+    db.session.add(post)
+    db.session.commit()
+    return redirect('/register')
