7a289ac50666aca16da9bc7131a60db9494ff3d6
krysia <krystyna.m.piatkowska@gmail.com>
2018-04-24 11:59:59 +0200

    deleted some comments with unused bcrypt

--- diet_manager/__init__.py
+++ diet_manager/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from config import Config
 
-# from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,7 +19,6 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login_user_dm'
 
-# bcrypt = Bcrypt()
 
 app.static_path = path.join(path.abspath(__file__), 'static')
 
