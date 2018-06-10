735f4aae6be318bf88e2faed632a82dc9ba78224
krysia <krystyna.m.piatkowska@gmail.com>
2018-06-10 21:11:16 +0200

    requirements rename, port to 8080

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from diet_manager import app
 
 if __name__ == '__main__':
     app.secret_key = 'my_secret_key'
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
