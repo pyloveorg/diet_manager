b51ae80a6befe9c16e7f0921083c9989e111d450
krysia <krystyna.m.piatkowska@gmail.com>
2018-06-10 21:24:12 +0200

    dirty hack: init db if doesn't exist

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diet_manager import app
+import os
 
+from init_db import db_start
 
 if __name__ == '__main__':
+    if not os.path.isfile("diet_manager.db"):
+        db_start()
     app.secret_key = 'my_secret_key'
     app.run(host='0.0.0.0', port=8080)
     #app.run(debug=True)
